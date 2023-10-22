d231fe590610ef4067da3d1f3da5cbd65b9ffe6e
Jerry Fan <huanyueyaoqin@qq.com>
2023-10-22 21:38:38 +0800

    chore: Optimize styles

--- chat2db-client/src/components/CascaderDB/index.tsx
+++ chat2db-client/src/components/CascaderDB/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Divider, Select, Typography } from 'antd';
+import { Divider, Select, Spin, Typography } from 'antd';
 import connection from '@/service/connection';
 import cs from 'classnames';
 import styles from './index.less';
 import Iconfont from '../Iconfont';
 import { databaseMap } from '@/constants/database';
+import { set } from 'lodash';
 
 interface IProps {
   className?: string;
@@ -26,6 +27,10 @@ function CascaderDB(props: IProps) {
 
   const [schemaOptions, setSchemaOptions] = useState<IOption[]>([]);
   const [curSchemeName, setCurSchemeName] = useState<string>('');
+
+  const [isDataSourceLoading, setIsDataSourceLoading] = useState<boolean>(false);
+  const [isDatabaseLoading, setIsDatabaseLoading] = useState<boolean>(false);
+  const [isSchemaLoading, setIsSchemaLoading] = useState<boolean>(false);
 
   useEffect(() => {
     loadDataSource();
@@ -55,7 +60,7 @@ function CascaderDB(props: IProps) {
     setCurDatabaseName(value);
     setSchemaOptions([]);
     setCurSchemeName('');
-    
+
     props.onChange &&
       props.onChange({
         dataSourceId: curDataSourceId!,
@@ -74,26 +79,33 @@ function CascaderDB(props: IProps) {
   /** 加载DataSource数据 */
   const loadDataSource = async () => {
     // 请求 dataSource 数据
-    const dataSourceList = await connection.getList({
-      pageNo: 1,
-      pageSize: 999,
-      refresh: true,
-    });
-    const formattedData = (dataSourceList?.data || []).map((item) => ({
-      ...item,
-      key: `dataSource-${item.id}`,
-      value: item.id,
-      label: (
-        <div className={styles.optionItem}>
-          <Iconfont className={styles.optionItemIcon} code={databaseMap[item.type]?.icon} />
-          <div className={styles.optionItemText}>{item.alias}</div>
-        </div>
-      ),
-    }));
-    setDataSourceOptions(formattedData);
+    setIsDataSourceLoading(true);
+    try {
+      const dataSourceList = await connection.getList({
+        pageNo: 1,
+        pageSize: 999,
+        refresh: true,
+      });
+      const formattedData = (dataSourceList?.data || []).map((item) => ({
+        ...item,
+        key: `dataSource-${item.id}`,
+        value: item.id,
+        label: (
+          <div className={styles.optionItem}>
+            <Iconfont className={styles.optionItemIcon} code={databaseMap[item.type]?.icon} />
+            <div className={styles.optionItemText}>{item.alias}</div>
+          </div>
+        ),
+      }));
+      setDataSourceOptions(formattedData);
 
-    if (curDataSourceId === undefined) {
-      setCurDataSourceId(formattedData[0]?.value);
+      if (curDataSourceId === undefined) {
+        setCurDataSourceId(formattedData[0]?.value);
+      }
+    } catch (error) {
+      console.error('get dataSourceList error', error);
+    } finally {
+      setIsDataSourceLoading(false);
     }
   };
 
@@ -102,25 +114,33 @@ function CascaderDB(props: IProps) {
     if (curDataSourceId === undefined) {
       return;
     }
-    const databaseList = await connection.getDBList({
-      dataSourceId: curDataSourceId,
-    });
 
-    const formattedData = (databaseList || []).map((item) => ({
-      ...item,
-      key: `database-${item.name}`,
-      value: item.name,
-      label: (
-        <div className={styles.optionItem}>
-          <Iconfont className={styles.optionItemIcon} code="&#xe744;" />
-          <div className={styles.optionItemText}>{item.name}</div>
-        </div>
-      ),
-    }));
+    setIsDatabaseLoading(true);
+    try {
+      const databaseList = await connection.getDBList({
+        dataSourceId: curDataSourceId,
+      });
 
-    setDatabaseOptions(formattedData);
-    if (!curDatabaseName) {
-      setCurDatabaseName(formattedData[0]?.value);
+      const formattedData = (databaseList || []).map((item) => ({
+        ...item,
+        key: `database-${item.name}`,
+        value: item.name,
+        label: (
+          <div className={styles.optionItem}>
+            <Iconfont className={styles.optionItemIcon} code="&#xe744;" />
+            <div className={styles.optionItemText}>{item.name}</div>
+          </div>
+        ),
+      }));
+
+      setDatabaseOptions(formattedData);
+      if (!curDatabaseName) {
+        setCurDatabaseName(formattedData[0]?.value);
+      }
+    } catch (error) {
+      console.log('get databaseList error', error);
+    } finally {
+      setIsDatabaseLoading(false);
     }
   };
 
@@ -128,91 +148,111 @@ function CascaderDB(props: IProps) {
     if (curDataSourceId === undefined || !curDatabaseName) {
       return;
     }
-    const schemaList = await connection.getSchemaList({
-      dataSourceId: curDataSourceId,
-      databaseName: curDatabaseName,
-      refresh: false,
-    });
+    setIsSchemaLoading(true);
+    try {
+      const schemaList = await connection.getSchemaList({
+        dataSourceId: curDataSourceId,
+        databaseName: curDatabaseName,
+        refresh: false,
+      });
 
-    const formattedData = (schemaList || []).map((item) => ({
-      ...item,
-      key: `schema-${item.name}`,
-      value: item.name,
-      label: (
-        <div className={styles.optionItem}>
-          <Iconfont className={styles.optionItemIcon} code="&#xe696;" />
-          <div className={styles.optionItemText}>{item.name}</div>
-        </div>
-      ),
-    }));
+      const formattedData = (schemaList || []).map((item) => ({
+        ...item,
+        key: `schema-${item.name}`,
+        value: item.name,
+        label: (
+          <div className={styles.optionItem}>
+            <Iconfont className={styles.optionItemIcon} code="&#xe696;" />
+            <div className={styles.optionItemText}>{item.name}</div>
+          </div>
+        ),
+      }));
 
-    setSchemaOptions(formattedData);
-    if (!curSchemeName) {
-      setCurSchemeName(formattedData[0]?.value);
+      setSchemaOptions(formattedData);
+      if (!curSchemeName) {
+        setCurSchemeName(formattedData[0]?.value);
+      }
+    } catch (error) {
+      console.log('get schemaList error', error);
+    } finally {
+      setIsSchemaLoading(false);
     }
   };
 
   return (
     <div className={cs(props.className, styles.cascaderDB)}>
-      <Select
-        bordered={false}
-        placeholder="请选择链接"
-        showSearch
-        popupMatchSelectWidth={false}
-        options={dataSourceOptions}
-        value={curDataSourceId}
-        onChange={handleChangeDataSource}
-        className={styles.select}
-        dropdownRender={(menu) => (
-          <>
-            <div style={{ padding: '8px 16px' }}>
-              <Typography.Title level={4}>链接源</Typography.Title>
-              <Divider style={{ margin: '4px 0' }} />
-            </div>
-            {menu}
-          </>
-        )}
-      />
-      <Select
-        bordered={false}
-        placeholder="请选择数据库"
-        showSearch
-        popupMatchSelectWidth={false}
-        options={databaseOptions}
-        value={curDatabaseName}
-        onChange={handleChangeDatabase}
-        className={styles.select}
-        dropdownRender={(menu) => (
-          <>
-            <div style={{ padding: '8px 16px' }}>
-              <Typography.Title level={4}>数据库</Typography.Title>
-              <Divider style={{ margin: '4px 0' }} />
-            </div>
-            {menu}
-          </>
-        )}
-      />
-      {!!schemaOptions.length && (
+      <Spin spinning={isDataSourceLoading}>
         <Select
           bordered={false}
-          placeholder="请选择Schema"
+          placeholder="请选择链接"
           showSearch
           popupMatchSelectWidth={false}
-          options={schemaOptions}
-          value={curSchemeName}
-          onChange={handleChangeSchema}
+          options={dataSourceOptions}
+          value={curDataSourceId}
+          onChange={handleChangeDataSource}
           className={styles.select}
           dropdownRender={(menu) => (
             <>
               <div style={{ padding: '8px 16px' }}>
-                <Typography.Title level={4}>Schema</Typography.Title>
+                <Typography.Title level={4}>链接源</Typography.Title>
                 <Divider style={{ margin: '4px 0' }} />
               </div>
               {menu}
             </>
           )}
         />
-      )}
+      </Spin>
+
+      <Spin spinning={isDatabaseLoading}>
+        {isDatabaseLoading ? (
+          <div style={{ width: '100px' }} />
+        ) : (
+          <Select
+            bordered={false}
+            placeholder="请选择数据库"
+            showSearch
+            popupMatchSelectWidth={false}
+            options={databaseOptions}
+            value={curDatabaseName}
+            onChange={handleChangeDatabase}
+            className={styles.select}
+            dropdownRender={(menu) => (
+              <>
+                <div style={{ padding: '8px 16px' }}>
+                  <Typography.Title level={4}>数据库</Typography.Title>
+                  <Divider style={{ margin: '4px 0' }} />
+                </div>
+                {menu}
+              </>
+            )}
+          />
+        )}
+      </Spin>
+
+      <Spin spinning={isSchemaLoading}>
+        {!!schemaOptions.length && (
+          <Select
+            bordered={false}
+            placeholder="请选择Schema"
+            showSearch
+            popupMatchSelectWidth={false}
+            options={schemaOptions}
+            value={curSchemeName}
+            onChange={handleChangeSchema}
+            className={styles.select}
+            dropdownRender={(menu) => (
+              <>
+                <div style={{ padding: '8px 16px' }}>
+                  <Typography.Title level={4}>Schema</Typography.Title>
+                  <Divider style={{ margin: '4px 0' }} />
+                </div>
+                {menu}
+              </>
+            )}
+          />
+        )}
+        {isSchemaLoading && <div style={{ width: '100px' }} />}
+      </Spin>
     </div>
   );
 }
